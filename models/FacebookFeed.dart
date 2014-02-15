d8d2ecc7aa4a1a45d3b1be9c6e01f045ade65950
Vladimir Belohradsky <info@lweek.net>
2014-02-15 17:19:25 +0100

    Fake twitter feed
    Fake twitter feed for testing purpose

--- models/FacebookFeed.dart
+++ models/FacebookFeed.dart
@@ -3,8 +3,9 @@ library socialwall.models.feed.facebookfeed;
 
 import 'dart:core';
 import 'Feed.dart';
+import 'Post.dart';
 
 class FacebookFeed extends Feed
 {
-  
+  FacebookFeed(configuration) : super(configuration);
 }
--- models/GooglePlusFeed.dart
+++ models/GooglePlusFeed.dart
@@ -3,8 +3,9 @@ library socialwall.models.feed.googleplusfeed;
 
 import 'dart:core';
 import 'Feed.dart';
+import 'Post.dart';
 
 class GooglePlusFeed extends Feed
 {
-  
+  GooglePlusFeed(configuration) : super(configuration);
 }
